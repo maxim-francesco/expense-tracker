006ab9055608aa8f2440631bff5e3293851278f6
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-02-19 20:49:10 +0200

    inject Service and create onSubmit

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -18,11 +19,16 @@ export class AuthComponent {
 
   constructor(
     private router: Router,
+    private authService: AuthService
   ) { }
 
 
   toggleMode() {
     this.isRegistering = !this.isRegistering;
+  }
+
+  onSubmit(){
+    
   }
 
 }
