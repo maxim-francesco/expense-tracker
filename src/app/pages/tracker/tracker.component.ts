42dfab9c49dbcb54531d706d15eab66a8616b779
Marita-Thuri Anna <maritathurianna@gmail.com>
2025-02-20 22:20:57 +0200

    weekly overview with dummy data

--- src/app/pages/tracker/tracker.component.ts
+++ src/app/pages/tracker/tracker.component.ts
@@ -19,7 +19,7 @@ export class TrackerComponent implements OnInit {
 
   constructor(private trackerConfigService: TrackerConfigService) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.trackerConfigService.getWeekdays().subscribe((config: TrackerConfig) => {
       this.days = config.weekdays.map(day => day.name);
       this.selectedDay = this.days.length > 0 ? this.days[0] : '';
@@ -39,4 +39,31 @@ export class TrackerComponent implements OnInit {
     this.newCategory = '';
   }
 
+
+  // Dummy expense data and functions
+
+  showWeeklyOverview = false;
+
+  expenses = [
+    { day: 'Monday', amount: 20 },
+    { day: 'Monday', amount: 15 },
+    { day: 'Tuesday', amount: 30 },
+    { day: 'Wednesday', amount: 25 },
+    { day: 'Wednesday', amount: 10 },
+    { day: 'Thursday', amount: 40 }
+  ];
+
+  toggleWeeklyOverview(): void {
+    this.showWeeklyOverview = !this.showWeeklyOverview;
+  }
+
+  getTotalWeeklyExpenses(): number {
+    return this.expenses.reduce((total, expense) => total + expense.amount, 0);
+  }
+
+  getTotalForDay(day: string): number {
+    return this.expenses
+      .filter(expense => expense.day === day)
+      .reduce((total, expense) => total + expense.amount, 0);
+  }
 }
