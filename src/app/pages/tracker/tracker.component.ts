871612508e6314de4a80e301ce6bf021b91a0fe9
Marita-Thuri Anna <maritathurianna@gmail.com>
2025-02-20 21:53:56 +0200

    things added  + dynamic weekdays

--- src/app/pages/tracker/tracker.component.ts
+++ src/app/pages/tracker/tracker.component.ts
@@ -1,13 +1,42 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrackerConfigService, TrackerConfig } from '../../services/tracker-config.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tracker',
-  imports: [NgFor],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './tracker.component.html',
-  styleUrl: './tracker.component.css'
+  styleUrls: ['./tracker.component.css']
 })
-export class TrackerComponent {
-  days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-  selectedDay = "Monday";
+export class TrackerComponent implements OnInit {
+  days: string[] = [];
+  selectedDay = '';
+  categories: string[] = [];
+  newCategory = '';
+  showCategoryPopup = false;
+
+  constructor(private trackerConfigService: TrackerConfigService) { }
+
+  ngOnInit(){
+    this.trackerConfigService.getWeekdays().subscribe((config: TrackerConfig) => {
+      this.days = config.weekdays.map(day => day.name);
+      this.selectedDay = this.days.length > 0 ? this.days[0] : '';
+    });
+  }
+
+  toggleCategoryPopup() {
+    this.showCategoryPopup = !this.showCategoryPopup;
+    this.newCategory = '';
+  }
+
+  addCategory() {
+    if (this.newCategory.trim() && !this.categories.includes(this.newCategory.trim())) {
+      this.categories.unshift(this.newCategory.trim());
+    }
+    this.showCategoryPopup = false;
+    this.newCategory = '';
+  }
+
 }
