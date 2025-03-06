039e4f1d12afad352b0d9a39d1e1ab624356167b
GengiuRobert <fncrobert3@gmail.com>
2025-03-06 11:35:35 +0200

    html template and styles for spinner

--- src/app/components/loading-spinner/loading-spinner.component.ts
+++ src/app/components/loading-spinner/loading-spinner.component.ts
@@ -1,11 +1,14 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './loading-spinner.component.html',
   styleUrl: './loading-spinner.component.css'
 })
 export class LoadingSpinnerComponent {
+
+  public isLoading = false;
 
 }
