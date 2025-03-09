431d3136d80b717ce70d8f0faeff6a7af31e3b4a
antoniaalbu <117073058+antoniaalbu@users.noreply.github.com>
2025-03-09 17:17:36 +0200

    Added final styling features

--- src/app/pages/aboutus/aboutus.component.ts
+++ src/app/pages/aboutus/aboutus.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './aboutus.component.css'
 })
 export class AboutusComponent {
-
+  scrollToIntro(): void {
+    const element = document.querySelector(".intro-text");
+    if (element) {
+      const offset = element.getBoundingClientRect().top + window.scrollY - 20; // Adjust for header space if needed
+      window.scrollTo({ top: offset, behavior: "smooth" });
+    }
+  }
+  
 }
